4aec6f931949c94eb0321b2a95d110d9f83bce3e
Yoon Sang <ys815h@naver.com>
2023-07-24 14:30:21 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,15 +1,10 @@
--- 조건 1. 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty
--- 동물의 아이디와 이름, 성별 및 중성화 여부를 조회 (아이디 순으로 조회)
--- ANIMAL_ID, NAME, SEX_UPON_INTAKE
+-- 조건 1. 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty 인 동물
+-- ANIMAL_ID	NAME	SEX_UPON_INTAKE 조회
+-- 결과는 아이디 순으로 조회
 
 -- SELECT * FROM ANIMAL_INS
 
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME = 'Lucy'
-OR NAME = 'Ella'
-OR NAME = 'Pickle'
-OR NAME = 'Rogan'
-OR NAME = 'Sabrina'
-OR NAME = 'Mitty'
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
 ORDER BY ANIMAL_ID
