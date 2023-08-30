b15f5cf1042b194d825d778872c35e5f381ff129
Yoon Sang <ys815h@naver.com>
2023-08-30 18:33:43 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,9 +1,12 @@
 def solution(strings, n):
     
     st = [list(i) for i in strings]
+    print(st)
     
     st.sort(key=lambda x:(x[n],x))
-
+    print(st)
+    
+    strings.sort(key=lambda x:(x[n],x))
     
     answer = [''.join(i) for i in st]
-    return answer
+    return strings
