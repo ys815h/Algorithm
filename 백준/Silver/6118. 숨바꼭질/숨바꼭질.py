9d2b2700128f8fa20f0ede392d1e7a7215076b8d
Yoon Sang <ys815h@naver.com>
2023-09-12 21:15:49 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 37708 KB -BaekjoonHub

--- 백준/Silver/6118. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.py
@@ -18,12 +18,11 @@ for g in range(1, N+1):
     graph[g].sort()
 
 visit = [0] * (N+1)
-visit[1] = 1
 st = []
-cnt = 1
 def bfs(v):
     q = deque()
     q.append(v)
+    visit[1] = 1
     while q:
         v = q.popleft()
 
@@ -31,15 +30,20 @@ def bfs(v):
             if visit[j] == 0:
                 visit[j] = visit[v] + 1
                 q.append(j)
-                st.append([j, visit[j]-1])
+                # st.append([j, visit[j]-1])
 
 bfs(1)
-st.sort()
-st.sort(reverse=True, key=lambda x:x[1])
-for i in range(len(st)):
-    if i > 0:
-        if st[i][1] == st[i-1][1]:
-            cnt += 1
-        else:
-            break
-print(st[0][0], st[0][1], cnt)
+
+m = max(visit)
+print(visit.index(m), m-1, visit.count(m))
+
+# cnt = 1
+# for i in range(len(st)):
+#     if i > 0:
+#         if st[i][1] == st[i-1][1]:
+#             cnt += 1
+#         else:
+#             break
+# st.sort()
+# st.sort(reverse=True, key=lambda x:(x[1]))
+# print(st[0][0], st[0][1], cnt)
