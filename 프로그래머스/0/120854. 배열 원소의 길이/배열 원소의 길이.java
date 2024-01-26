837bef08c5d61eb7350b919a64905ab87dce06da
Yoon Sang <ys815h@naver.com>
2024-01-26 20:18:29 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        
+        for (int i=0; i<strlist.length; i++) {
+            answer[i] = strlist[i].length();
+        }
+        
+        return answer;
+    }
+}
