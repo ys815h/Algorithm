a9e6c5f6144d8d38027dee95f0f005163ced8f82
Yoon Sang <ys815h@naver.com>
2024-01-30 20:41:50 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.18 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.split("");
+        
+        for (int i=0; i<arr.length; i++) {
+            try {
+                answer += Integer.parseInt(arr[i]);
+            } catch (Exception ex) {
+                continue;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
