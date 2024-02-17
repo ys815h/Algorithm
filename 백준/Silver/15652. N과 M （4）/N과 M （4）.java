b68f07ea91a09b2e6da140eec666cd249e6d5d37
Yoon Sang <ys815h@naver.com>
2024-02-17 19:50:51 +0900

    [Silver III] Title: N과 M (4), Time: 248 ms, Memory: 21288 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n, m, s;
+	static int[] graph;
+	static Stack<Integer> st = new Stack<>();
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(token.nextToken());
+		m = Integer.parseInt(token.nextToken());
+		s = 1;
+		
+		dfs(s);
+		System.out.println(sb);
+	}
+	public static void dfs(int s) {
+		if (st.size() == m) {
+			for(int val : st) {
+				sb.append(val + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i=s; i<=n; i++) {
+			st.push(i);
+			dfs(i);
+			st.pop();
+		}
+		return;
+	}
+	
+}
