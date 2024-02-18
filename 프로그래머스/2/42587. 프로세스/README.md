3bb8dbc3dc3884a510236d5f8d35d13128cdcdf3
Yoon Sang <ys815h@naver.com>
2024-02-18 22:06:18 +0900

    [level 2] Title: 프로세스, Time: 0.59 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 73.5 MB, 시간: 0.67 ms
+메모리: 74.1 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 0일 21:39:17
+2024년 2월 0일 22:6:17
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,24 +3,18 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 1;
         Queue<Integer> p_q = new LinkedList<>();
-        Queue<Integer> m_q = new LinkedList<>();
-        int[] max = Arrays.copyOf(priorities, priorities.length);
-        Arrays.sort(max);
-        
-        for(int i=0; i<priorities.length; i++) {
-            p_q.offer(priorities[i]);
+        for (int i : priorities) {
+            p_q.offer(i);
         }
-        for(int i=max.length-1; i>=0; i--) {
-            m_q.offer(max[i]);
-        }
+        Arrays.sort(priorities);
+        int p = priorities.length-1;
         
         while(!p_q.isEmpty()) {
-            if(p_q.peek() == m_q.peek() && location == 0) break;
-            else {
-                // 우선순위 높은 프로세스일 때
-                if(p_q.peek() == m_q.peek()) {
+            if(p_q.peek() == priorities[p] && location == 0) break;
+            else{
+                if(p_q.peek() == priorities[p]) {
                     p_q.poll();
-                    m_q.poll();
+                    p--;
                     answer++;
                 } else {
                     p_q.offer(p_q.poll());
@@ -32,6 +26,36 @@ class Solution {
                 }
             }
         }
+        
+//         Queue<Integer> m_q = new LinkedList<>();
+//         int[] max = Arrays.copyOf(priorities, priorities.length);
+//         Arrays.sort(max);
+        
+//         for(int i=0; i<priorities.length; i++) {
+//             p_q.offer(priorities[i]);
+//         }
+//         for(int i=max.length-1; i>=0; i--) {
+//             m_q.offer(max[i]);
+//         }
+        
+//         while(!p_q.isEmpty()) {
+//             if(p_q.peek() == m_q.peek() && location == 0) break;
+//             else {
+//                 // 우선순위 높은 프로세스일 때
+//                 if(p_q.peek() == m_q.peek()) {
+//                     p_q.poll();
+//                     m_q.poll();
+//                     answer++;
+//                 } else {
+//                     p_q.offer(p_q.poll());
+//                 }
+//                 if(location == 0) {
+//                     location = p_q.size()-1;
+//                 } else {
+//                     location--;
+//                 }
+//             }
+//         }
         
         return answer;
     }
